0ffe1339b1ed9c9c9542d43cf54cbbbb0836e1cc
David Rodriguez <davidrodrigueznolimit@gmail.com>
2021-05-26 18:30:31 -0500

    Updates to file structure, added .json files

--- index.js
+++ index.js
@@ -3,23 +3,40 @@ import * as state from "./store";
 
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
 
 const router = new Navigo(window.location.origin);
 
+// router.hooks({
+//   before: (done, params) => {
+//     axios
+//       .get("https://jsonplaceholder.typicode.com/posts")
+//       // handle the response from the API
+//       .then(response => {
+//         // for each post in the response Array,
+//         response.data.forEach(post => {
+//           // add it to state.Blog.posts
+//           state.Blog.posts.push(post);
+//         });
+//         done();
+//       });
+//   }
+// });
+
 router
   .on({
-    ":page": (params) => render(state[capitalize(params.page)]),
-    "/": () => render(state.Home),
+    ":page": params => render(state[capitalize(params.page)]),
+    "/": () => render(state.Home)
   })
   .resolve();
 
 function render(st = state.Home) {
   document.querySelector("#root").innerHTML = `
-  ${Header(st)}
-  ${Nav(state.Links)}
-  ${Main(st)}
-  ${Footer()}
-`;
+    ${Header(st)}
+    ${Nav(state.Links)}
+    ${Main(st)}
+    ${Footer()}
+  `;
 
   router.updatePageLinks();
 
@@ -28,36 +45,17 @@ function render(st = state.Home) {
 
 function addEventListeners(st) {
   // add event listeners to Nav items for navigation
-  document.querySelectorAll("nav a").forEach((navLink) =>
-    navLink.addEventListener("click", (event) => {
+  document.querySelectorAll("nav a").forEach(navLink =>
+    navLink.addEventListener("click", event => {
       event.preventDefault();
       render(state[event.target.title]);
     })
   );
 
-  // add menu toggle to bars icon in nav bar
+  // menu toggle to hamburger
   document
     .querySelector(".fa-bars")
     .addEventListener("click", () =>
       document.querySelector("nav > ul").classList.toggle("hidden--mobile")
     );
-
-  // event listener for the the photo form
-  if (st.view === "Form") {
-    document.querySelector("form").addEventListener("submit", (event) => {
-      event.preventDefault();
-      // convert HTML elements to Array
-      let inputList = Array.from(event.target.elements);
-      // remove submit button from list
-      inputList.pop();
-      // construct new picture object
-      let newPic = inputList.reduce((pictureObject, input) => {
-        pictureObject[input.name] = input.value;
-        return pictureObject;
-      }, {});
-      // add new picture to state.Gallery.pictures
-      state.Gallery.pictures.push(newPic);
-      render(state.Gallery);
-    });
-  }
 }
