f0f39275cdeaa85a74912e8c8eedcb99f810c09e
David Rodriguez <davidrodrigueznolimit@gmail.com>
2021-06-30 19:10:48 -0500

    Edit down text content on About view

--- components/views/About.js
+++ components/views/About.js
@@ -14,14 +14,8 @@ export default () => html`
       membership and offer you the flexibility and freedom to try the big box
       gym up the street or your neighbor's sweet garage setup, all without
       signing your life away. Pay for what you use, and never pay when you
-      don't. Can't justify the financial cost or the space to build a home gym?
-      Can't blame you. Let's face it... you're only going to use it once a day
-      at most. So why not try out your neighbor's garage setup? Or how about
-      that cool set of weights the folks up the street have in their back yard?
-      Done. Want to try a new piece of equipment? We've got you. Want to explore
-      a new fitness craze? Not a problem. Not in love with the last place you
-      tried? No sweat - just don't go back. It's that's easy. It's total fitness
-      freedom. Just the way it ought to be.
+      don't. It's that's easy. It's total fitness freedom. Just the way it ought
+      to be.
     </p>
   </div>
   <div class="container aboutBox">
@@ -52,11 +46,8 @@ export default () => html`
       checking out your facility. Every cost matters, and it all falls on you.
       That's why AnyGym is building a network of qualified leads for you. Tell
       us when you can be available, and we'll start booking you appointments.
-      Personal training? Check. Gym tours? Check. You know all those hours your
-      gym is empty, but you're still paying rent on it? Let us help you foot
-      that bill by utilizing that space for walk ins to enjoy the facility on an
-      as-needed basis and discover why your place is the best place. Fitness
-      fans all over your area need your gym. Let us help you show them why.
+      Fitness fans all over your area need your gym. Let us help you show them
+      why.
     </p>
   </div>
   <div class="container aboutBox">
@@ -65,22 +56,15 @@ export default () => html`
       Home Gym Owners
     </h3>
     <p>
-      That's a pretty sweet setup you've got there. You paid good money for that
-      new reflective video on demand thing. Or maybe you have the absolute
-      epitome of the quintessential weight bench and barbell setup. 800 square
-      feet of pure sweaty bliss. Too bad it's just collecting dust 23 hours a
-      day, 7 days a week. What if you could turn that space into extra cash
-      without losing your tiny fitness paradise? That's where we come in. After
-      all, if you can rent a room in your house to other people, or the studio
-      apartment in your basement that your son abandoned when he went off to
-      college, why can't you do the same with your temple of muscle? Here at
-      AnyGym, you'll soon start bringing in extra money as your just reward for
-      committing that space and cash toward your healthier future. Don't let
-      your exercise palace just waste away in your off hours. Get paid for
-      building something great. Just sign up, give us some details, and tell us
-      when other fit fanatics are free to stop by, and we'll start booking it
-      and make sure you reap more benefits than just that impressive physique of
-      yours.
+      That's a pretty sweet setup you've got there. Too bad it's just collecting
+      dust 23 hours a day, 7 days a week. That's where we come
+      in. After all, if you can rent a room in your house to other people; why
+      can't you do the same with your temple of muscle? Here at AnyGym, you'll
+      soon start bringing in extra money as your just reward for committing that
+      space and cash toward your healthier future. Just sign up, give us some
+      details, and tell us when other fit fanatics are free to stop by, and
+      we'll start booking it and make sure you reap more benefits than just that
+      impressive physique of yours.
     </p>
   </div>
   <h3>AnyGym is democratizing fitness for all.</h3>
